4513b1acbd9a74b6737ddd62b4bfcb8ac0e5cf55
Kenzie Togami <ket1999@gmail.com>
2014-03-15 16:26:52 -0700

    Files.

---
+++ src/com/techshroom/bukkit/plugin/TestPlugin3615.java
@@ -0,0 +1,20 @@
+package com.techshroom.bukkit.plugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TestPlugin3615 extends JavaPlugin implements Listener {
+    private boolean cancel = false;
+
+    public void onEnable() {
+        getLogger().info("Loading test plugin for Bukkit-3615...");
+        getLogger().info("Resource pack is '"+Bukkit.getResourcePack()+"'");
+        getLogger().info("Loaded.");
+    }
+
+    public void onDisable() {
+        getLogger().info("Unloading test plugin for Bukkit-3615...");
+        getLogger().info("Unloaded.");
+    }
+}
