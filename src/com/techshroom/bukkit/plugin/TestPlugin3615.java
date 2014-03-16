4a1b6445a5bceb2400417f52cf68c4d381c9e7c6
Kenzie Togami <ket1999@gmail.com>
2014-03-15 19:00:40 -0700

    Something formatted

--- src/com/techshroom/bukkit/plugin/TestPlugin3615.java
+++ src/com/techshroom/bukkit/plugin/TestPlugin3615.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class TestPlugin3615 extends JavaPlugin implements Listener {
     public void onEnable() {
         getLogger().info("Loading test plugin for Bukkit-3615...");
-        getLogger().info("Resource pack is '"+Bukkit.getResourcePack()+"'");
+        getLogger().info("Resource pack is '" + Bukkit.getResourcePack() + "'");
         getLogger().info("Loaded.");
     }
 
